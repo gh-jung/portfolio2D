6e9995278814882314c30b29a0511fd0c7d9f8ee
정구현 <201003145.mcas@gmail.com>
2019-09-11 00:52:57 +0900

    싱글턴 패턴이 적용된 게임 메니저 생성

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    public static int SIZE_X = 20;
+    public static int SIZE_Y = 20;
+    public static string PLAYER = "player";
+    public static string BULLET_PATH = "Prefabs/bullet";
+
+    public ObjectInfo CreateObj(string obj)
+    {
+        ObjectInfo temp = Resources.Load("Scriptable/Characters/" + obj) as ObjectInfo;
+        return temp;
+    }
+}
