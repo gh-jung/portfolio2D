db6cc7da1d5f31e23f3a68c99129e891680208f1
jeong.guhyeon <werty9611@naver.com>
2019-10-05 02:51:32 +0900

    버튼 이미지 추가 및 버튼 on/off 기능 추가
    추가 구현 기능 : 클릭 시 사운드, 화면전환 스프라이트, 메뉴 텍스트 추가

--- Assets/Script/OnClcikUIButton.cs
+++ Assets/Script/OnClcikUIButton.cs
@@ -2,12 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class OnClcikUIButton : MonoBehaviour
 {
     public string sceneName;
+    private UIButton uIButton;
+    private UI2DSprite uI2DSprite;
+    public bool isDisabled;
+
+    private void Start()
+    {
+        uI2DSprite = GetComponent<UI2DSprite>();
+        uIButton = GetComponent<UIButton>();
+        if (isDisabled)
+        {
+            //uIButton.SetState(UIButtonColor.State.Disabled, isDisabled);
+            uIButton.isEnabled = !isDisabled;
+        }
+    }
+
     public void OnClick()
     {
-        SceneManager.LoadScene(sceneName);
+        if (!isDisabled)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
     }
 }
