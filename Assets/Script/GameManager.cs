06cd6e8b3a8092c7766981a3c0dd5833aa28719c
jeong.guhyeon <werty9611@naver.com>
2019-10-06 01:08:56 +0900

    적 무한 생성 로지 구현
    추가 구현 : 타일이 아래쪽 일수록 화면 뒤에 배치되도록 수정

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -15,6 +15,13 @@ public class GameManager : Singleton<GameManager>
 
     public List<ObjectController> enemys = new List<ObjectController>();
 
+    public GameObject enemy;
+    public Transform enemysParent;
+
+    public float respawnTime = 3;
+    private float currentTime;
+    private int respawnCount = 0;
+
     public static ObjectInfo LoadScriptable(string obj)
     {
         ObjectInfo temp = Resources.Load("Scriptable/" + obj) as ObjectInfo;
@@ -24,6 +31,34 @@ public class GameManager : Singleton<GameManager>
     public static GameObject LoadBullet(string path) 
     {
         return Instantiate(Resources.Load(GameManager.BULLET_PATH) as GameObject);
+    }
+
+    private void Start()
+    {
+        currentTime = 0;
+        respawnCount = 0;
+        respawnTime = 3;
+    }
+
+    private void Update()
+    {
+        if(currentTime > respawnTime)
+        {
+            GameObject enemyObj = Instantiate(enemy, enemysParent);
+            ObjectController enemyController = enemyObj.GetComponent<ObjectController>(); ;
+            int tileNumber = GetEmptyPos();
+
+            enemyObj.transform.position = TileManager.Instance.enemyMoveAbleTiles[tileNumber].transform.position;
+            enemys[tileNumber] = enemyController;
+            enemys[tileNumber].currentPos = tileNumber;
+            currentTime = 0;
+            respawnCount++;
+            if(respawnCount % 10 == 0)
+            {
+                respawnTime *= 0.9f;
+            }
+        }
+        currentTime += Time.deltaTime;
     }
 
     //플레이어와 같은 라인 찾고 해당라인에서 가장 가까운 적 찾기
@@ -48,5 +83,17 @@ public class GameManager : Singleton<GameManager>
     public void RemoveEnemy(int posIndex)
     {
         enemys[posIndex] = null;
+    }
+
+    public int GetEmptyPos()
+    {
+        
+        int value = UnityEngine.Random.Range(0, enemys.Count);
+        while(enemys[value] != null)
+        {
+            value = UnityEngine.Random.Range(0, enemys.Count);
+        }
+
+        return value;
     }
 }
