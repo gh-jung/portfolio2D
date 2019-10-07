80c7101dff5384f1ac91c28c680912dff11e686b
jeong.guhyeon <werty9611@naver.com>
2019-10-08 00:24:32 +0900

    타이틀 화면 버튼 추가 및 게임 화면 ui 버튼 애니매아션 수정

--- Assets/Script/OnClcikUIButton.cs
+++ Assets/Script/OnClcikUIButton.cs
@@ -7,16 +7,19 @@ public class OnClcikUIButton : MonoBehaviour
     public string sceneName;
     private UIButton uIButton;
     private UI2DSprite uI2DSprite;
+    private UILabel uILabel;
     public bool isDisabled;
 
     private void Start()
     {
         uI2DSprite = GetComponent<UI2DSprite>();
         uIButton = GetComponent<UIButton>();
+        uILabel = GetComponentInChildren<UILabel>();
         if (isDisabled)
         {
             //uIButton.SetState(UIButtonColor.State.Disabled, isDisabled);
             uIButton.isEnabled = !isDisabled;
+            uILabel.color = new Color(100.0f/ 255.0f, 100.0f / 255.0f, 100.0f / 255.0f);
         }
     }
 
@@ -26,5 +29,10 @@ public class OnClcikUIButton : MonoBehaviour
         {
             SceneManager.LoadScene(sceneName);
         }
+    }
+
+    public void OnHover(bool isOver)
+    {
+        uILabel.cachedTransform.localScale = isOver ? Vector3.one * 1.2f : Vector3.one;
     }
 }
